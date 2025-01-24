81c91d75ba0af58262f9aaae2b0a499d53cba20b
rodgers <rodgerstarrodgers@gmail.com>
2025-01-25 01:25:04 +0300

    intergrated frontend with add remove and get cart APIS

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -1,6 +1,7 @@
 // StoreContextProvider.js
 import {createContext, useEffect, useState} from "react";
 import axios from "axios";
+import header from "../components/Header/Header.jsx";
 
 
 export const StoreContext = createContext(null);
@@ -12,16 +13,22 @@ const StoreContextProvider = (props) => {
     const [token,setToken] = useState("");
     const [yarn_piece, setYarnPiece] = useState([])
 
-    const addToCart = (itemId) => {
+    const addToCart =  async (itemId) => {
         if (!cartItems[itemId]) {
             setCartItems((prev) => ({...prev, [itemId]: 1}))
         } else {
             setCartItems((prev) => ({...prev, [itemId]: prev[itemId] + 1}))
         }
+        if (token) {
+            await axios.post(url+"/api/cart/add",{itemId},{headers:{token}})
+        }
     }
 
-    const removeFromCart = (itemId) => {
+    const removeFromCart =  async (itemId) => {
         setCartItems((prev) => ({...prev, [itemId]: prev[itemId] - 1}))
+        if (token) {
+            await axios.post(url + "/api/cart/remove", { itemId }, { headers: { token } });
+        }
     }
 
 
@@ -41,12 +48,18 @@ const StoreContextProvider = (props) => {
         setYarnPiece(response.data.data)
     }
 
+    const loadCartData = async (token) =>{
+        const response = await axios.post(url+"/api/cart/get",{},{headers:{token}})
+        setCartItems(response.data.cartData);
+    }
+
     useEffect(() => {
 
         async function loadData() {
             await fetchYarnPiece();
             if (localStorage.getItem("token")) {
             setToken(localStorage.getItem("token"));
+            await loadCartData(localStorage.getItem("token"))
             }
         }
         loadData();
